fa3919434bd70daeb1e4c1cfae5daf644142f7c2
Sysmindtech Pvt LTD <sysmindtech@outlook.com>
2021-08-02 13:26:59 +0530

    Add project files.

---
+++ Student Management System1/Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using Student_Management_System1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Student_Managemen_System1.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly lindaEntities db = new lindaEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(user objchk)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                using (lindaEntities db = new lindaEntities())
+                {
+                    var obj = db.users.Where(a => a.username.Equals(objchk.username) && a.password.Equals(objchk.password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.id.ToString();
+                        Session["UserName"] = obj.username.ToString();
+                        return RedirectToAction("Index", "Home");
+                    }
+
+                    else
+                    {
+                        ModelState.AddModelError("", "The Username or Password Incorrect");
+
+                    }
+                }
+            }
+                  return View(objchk);
+
+             }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Login");
+
+
+
+        }
+
+            }
+           
+        }
+
+
